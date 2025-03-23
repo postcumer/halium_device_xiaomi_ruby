6bba88808e3cbcc14dbdfd79683adead4b77876e
postcumer <postcumer@tuta.io>
2025-03-23 17:55:23 +0530

    rubbish

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -xe
 export ARCH=arm64
 export LLVM=1
 
-[ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b main build
+[ -d build ] || git clone -b halium-12 https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b main build
 ./build/build.sh "$@"
